eb7ca3d24c848be8873927fc1e4551913d00c612
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-07-16 12:15:59 -0700

    remove nav menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
-import { api } from "@/utils/api";
-import { Button } from "@/components/ui/Button";
 import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div >
+    <div>
+      <Head>
+        <title>warlock</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div className="flex justify-center">
         <Navbar />
       </div>
