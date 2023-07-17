c3f42a937959cb1f909d6d531b356ed75a67ca63
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-07-17 11:58:19 -0700

    create form

--- src/components/site/CreateSiteForm.tsx
+++ src/components/site/CreateSiteForm.tsx
@@ -1,22 +1,46 @@
 import React from "react";
-import { Card, CardContent, CardHeader } from "../ui/Card";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CreateSiteValidator, type CreateSiteFormData } from "@/lib/validators/site";
+
+import { Card, CardContent } from "../ui/Card";
 import { Input } from "../ui/Input";
 import { Label } from "../ui/Label";
 import { Button } from "../ui/Button";
+import { api } from "@/utils/api";
 
 interface CreateSiteFormProps {
   userId: string;
 }
 
 export const CreateSiteForm: React.FC<CreateSiteFormProps> = ({ userId }) => {
+  const { mutate: createSite, isLoading } = api.site.createSite.useMutation();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<CreateSiteFormData>({
+    resolver: zodResolver(CreateSiteValidator),
+  });
+
+  const onSubmit = (data: CreateSiteFormData) => {
+    console.log("called")
+    createSite({ encryptionKeyHint: data.encryptionKeyHint, name: data.name });
+    reset();
+  };
+
   return (
     <div>
       <Card className="max-w-lg">
         <CardContent className="pt-5">
-          <form>
+          {/* eslint-disable-next-line */}
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div>
               <Label>site name</Label>
-              <Input placeholder="eg: twitter" />
+              <Input placeholder="eg: twitter" {...register("name")} />
               <p className="pt-2 text-xs text-gray-500">
                 site name is used to identify your site in the dashboard.
                 <br />
@@ -26,25 +50,28 @@ export const CreateSiteForm: React.FC<CreateSiteFormProps> = ({ userId }) => {
 
             <div className="pt-3">
               <Label>encryption key</Label>
-              <Input />
+              <Input {...register("encryptionKey")} />
               <p className="pt-2 text-xs text-red-500">
                 NOTE: we do not store your encryption key. if you lose your encryption key, you will
                 not be able to access the data related to this site. please keep your encryption key
-                safe and configure a good hint.
+                safe and configure a good hint. this cannot be updated.
               </p>
             </div>
 
             <div className="pt-3">
               <Label>encryption key hint</Label>
-              <Input placeholder="eg: my chemistry teacher&apos;s name + my cat&apos;s birthday" />
+              <Input
+                {...register("encryptionKeyHint")}
+                placeholder="eg: my chemistry teacher's name + my cat's birthday"
+              />
               <p className="pt-2 text-xs text-gray-500">
                 this hint will be shown to you if you forget your encryption key, please configure a
                 good, memorable hint.
               </p>
             </div>
 
-            <div className="pt-3 flex justify-center">
-              <Button size="lg">
+            <div className="flex justify-center pt-3">
+              <Button size="lg" type="submit">
                 create site
               </Button>
             </div>
--- src/lib/validators/site.ts
+++ src/lib/validators/site.ts
@@ -1,8 +1,18 @@
 import { z } from "zod";
 
 export const SiteValidator = z.object({
-  name: z.string().min(3).max(100).regex(/^[a-z0-9-]+$/i),
+  name: z
+    .string()
+    .min(3)
+    .max(100)
+    .regex(/^[a-z0-9-]+$/i),
   encryptionKeyHint: z.string().min(3).max(250),
-})
+});
 
-export type SiteRequest = z.infer<typeof SiteValidator>
+export type SiteRequest = z.infer<typeof SiteValidator>;
+
+export const CreateSiteValidator = SiteValidator.extend({
+  encryptionKey: z.string().min(3).max(75),
+});
+
+export type CreateSiteFormData = z.infer<typeof CreateSiteValidator>;
