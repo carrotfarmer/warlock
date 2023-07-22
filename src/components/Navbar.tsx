51a6cb3b2dba69f0121c006a714a1ac6ee846959
Dhruva Srinivas <dhruvas17068@gmail.com>
2023-07-22 13:26:10 -0700

    cool navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,12 @@ import { Button } from "./ui/Button";
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
+import { Avatar, AvatarImage } from "./ui/Avatar";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/Popover";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const Navbar = ({}) => {
   const { data: session } = useSession();
@@ -17,12 +23,26 @@ export const Navbar = ({}) => {
       </div>
       <div className="flex items-center">
         {session ? (
-          <Button
-            className="rounded-md border-none bg-purple-500 text-sm font-semibold text-white hover:bg-purple-600"
-            onClick={() => void signOut()}
-          >
-            sign out
-          </Button>
+          <div className="grid grid-cols-2">
+            <Popover>
+              <PopoverTrigger>
+                <Avatar>
+                  <AvatarImage src={session.user.image as string} alt={session.user.name as string} />
+                </Avatar>
+              </PopoverTrigger>
+
+              <PopoverContent className={cn(inter.className, "text-sm")}>
+                Logged in as: <p className="font-bold text-purple-300">{session.user.email as string}</p>
+              </PopoverContent>
+            </Popover>
+
+            <Button
+              className="rounded-md border-none bg-purple-500 text-sm font-semibold text-white hover:bg-purple-600"
+              onClick={() => void signOut()}
+            >
+              sign out
+            </Button>
+          </div>
         ) : (
           <Button
             className="rounded-md border-none bg-purple-500 text-sm font-semibold text-white hover:bg-purple-600"
